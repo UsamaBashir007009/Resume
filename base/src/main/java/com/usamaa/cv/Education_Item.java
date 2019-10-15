8219c51475bf63ebc12a1da7fda88da5995dd9d6
MAhmadRafique <ahmadrafique468@gmail.com>
2019-10-15 19:20:33 +0500

    Last Commit

---
+++ base/src/main/java/com/usamaa/cv/Education_Item.java
@@ -0,0 +1,18 @@
+package com.usamaa.cv;
+
+public class Education_Item {
+    public String institute_name;
+    public String Degree_name;
+    public String year;
+    public int Institue_Image;
+
+    public Education_Item(String iname, String dname, String yearr, int imageView)
+    {
+        institute_name=iname;
+        this.Degree_name=dname;
+        this.year=yearr;
+        this.Institue_Image=imageView;
+    }
+
+
+}
